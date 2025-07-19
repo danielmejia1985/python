c75f3d5987775e138625a3d2ca3fe6ab55fd810e
daniel_mejia <daniel.mejiapalacios1985@gmail.com>
2025-07-19 12:49:34 -0600

    actuaolizando practicas Python

---
+++ practicando_Python_mejiadev/cuestionario_seccion.py
@@ -0,0 +1,8 @@
+x = int(input("Ingrese un número: ")) # El usuario ingresa un número como cadena de caracter la función int() lo convierte a entero
+print(x * "5") # La función print() útilliza ese valor entero guardado en la varible x y es multiplicado con la cadeja cinco "5" e imprime por consola el número de veces dicho caracter cinco es decir, si el usuario ingresa 3 se imprimira 3 veces el caracter cinco 555.
+
+
+# En el siguiente fragmento de código el usuario debe ingresar un número y la función print() muestra por consola el tipo de dato que contine la variable x, esto con la función type(), es decir la salida por consola es <class 'str'> que quiere decir que es de tipo String o Cadena de caracteres.
+x = input("Ingresa un numero: ")
+print(type(x))
+
