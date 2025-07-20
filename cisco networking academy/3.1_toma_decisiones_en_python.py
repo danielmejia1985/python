e4ebb02e5e21c8be6d4b907ca1bb52068bddf4f6
daniel_mejia <daniel.mejiapalacios1985@gmail.com>
2025-07-20 06:11:56 -0600

    creando carpeta cisco networking academy

---
+++ cisco networking academy/3.1_toma_decisiones_en_python.py
@@ -0,0 +1,18 @@
+# 3.1 Sección 1 Toma de decisiones en Python
+"""
+
+En está sección aprenderemos sobre las SENTENCIAS CONDICIONALES y cómo usarlas para tomar decisiones en Python.
+
+3.1.1 Preguntas y Respuestas
+
+Un programador escribe un programa y el programa hace preguntas.
+Una  computadora ejecuta el programa y proporciona las respuestas. El programa debe ser capaz de reaccionar de acuerdo con las respuestas recibidas
+
+Afortunadamente, las computadoras solo conocen dos tipos de respuestas:
+-Si, es cierto.
+-No, esto es falso
+
+Nunca obtendrás una respuesta como Déjame pensar, no lo sé o probablemente sí, pero no lo sé con seguridad.
+
+Para hacer preguntas, Python utiliza un conjunto de operadores muy especiales.
+"""
