23ed249a706cb404501e1c7e1b61b72cfbf035f0
daniel_mejia <daniel.mejiapalacios1985@gmail.com>
2025-07-24 19:00:31 -0600

    Actualizando certificacion cisco python

--- cisco networking academy/3.2.8_las_sentencias_break_continue.py
+++ cisco networking academy/3.2.8_las_sentencias_break_continue.py
@@ -1,3 +1,5 @@
+import os
+os.system("clear")
 # Las sentencias break y continue
 '''
 
@@ -6,5 +8,33 @@ Hasta ahora, hemos tratado el cuerpo del bucle como una secuencia indivisible  e
 -Parece que no es necesario continuar el bucle en su totalidad; se debe abstener de seguir ejecutando el cuerpo del bucle e ir más allá.
 -Parece que necesitas comenzar el siguiente giro del bucle sin completar la ejecución del turno actual.
 
+Python proporciona dos instrucciones especiales para la implementación de estas dos tareas. Digamos por razones de precisión que su existencia en el lenguaje no sea necesaria - un programador experimentado puede codificar cualquier algoritmo si estas instrucciones.
 
+Tales adiciones, que no mejoranel poder expresivo del lenguaje, si no que solo simplifican  el trabajo del desarrollador, a veces se denomina, dulces sintácticos o azúcar sintáctica.
+
+Estas dos instrucciones son:
+-break: Sale del bucle inmediatamente, e incodicionalmente termina la operación del bucle; el programa comienza a ejecutar la instrucción más cercana después del cuerpo del bucle.
+-continue: Se comporta como si el programa hubiera llegado repentinamente al final del cuerpo; el siguiente turno se inicia y la expresión se prueba de inmediato.
+
+Ambas palabras son palabras claves reservadas.
+
+Ahora mostraremos dos ejemplos simples para ilustrar como funcionan las dos instrucciones.
 '''
+
+
+# Ejemplo 1, break
+print('La instrucción \"break\"')
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Dentro del bucle: ", i)
+print("fuera del bucle.")
+
+
+# Ejemplo 2, continue
+print('\nLa instruccion \"contunue\"')
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Dentro del bucle", i)
+print('Fuera del bucle.')
